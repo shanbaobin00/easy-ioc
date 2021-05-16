deac9440f2f62c2029171091534b7c3562aa73bc
shanbaobin00 <920641901@qq.com>
2021-05-16 22:29:43 +0800

    Update TransferServlet.java

--- src/main/java/com/eric/servlet/TransferServlet.java
+++ src/main/java/com/eric/servlet/TransferServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * @author 应癫
+ * @author
  */
 @WebServlet(name="transferServlet",urlPatterns = "/transferServlet")
 public class TransferServlet extends HttpServlet {
